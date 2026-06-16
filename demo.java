6354afe2f94effd6c9f14f2c19b9dbb3ac21768b
rishiverma04 <rishiverma1644@gmail.com>
2026-06-16 10:34:45 +0530

    Java OOPS concepts

---
+++ demo.java
@@ -0,0 +1,100 @@
+public class demo{
+      public static void main(String[] args) {
+         /*int a=5;
+         int b=10;
+
+         boolean c=(a==b);
+         System.out.println(c);
+
+         boolean d=(a!=b);
+         System.out.println(d);
+
+         boolean e=(a<b);
+         boolean f=(a>b);
+         System.out.println(e);
+         System.out.println(f);*/
+
+         /*int a=2,b=3;
+         int and=a&b;
+         int or=a|b;
+         int xorr=a^b;
+         int not=~a;
+         System.out.println(and+","+or+","+xorr+","+not);
+
+         int leftshift=1;
+         System.out.println(leftshift<<33);*/
+
+
+         /*int a=5,b=10,c=15;
+
+         boolean d=(a<b)&(b<c);
+         System.out.println(d);*/
+
+     
+        /* */
+       // while(i++<10){
+         //   System.out.print(" "+i);
+            
+        //}*/
+            
+            //Syntax//
+       /*do{
+            // do something
+       }while(expression);*/
+       
+       /*int i=0;
+      do{
+            System.out.println(i);
+            i++;
+      }while(i<=10);*/
+
+      /*for(int i=1;i<=10;i++){
+            System.out.print(" "+i);
+      }*/
+
+      /*for(int i=1;i<=10;i++){
+            for(int j=1;j<=i;j++){
+                  System.out.print("* ");
+            }
+            System.out.println();
+      }*/
+/*int x=101;
+      int newroll[]=new int[4];
+  for(int i=0;i<4;i++){
+      newroll[i]=x;
+      x++;
+
+  }
+  for(int i=0;i<4;i++){
+      System.out.println(newroll[i]);
+      
+    
+  }
+  System.out.println(newroll.length);
+  System.out.println(newroll[4]);*/
+
+
+  int marks[][]=new int[3][3];
+
+  marks[0][0]=50;
+  marks[0][1]=60;
+  marks[0][2]=70;
+
+  marks[1][0]=30;
+  marks[1][1]=40;
+  marks[1][2]=50;
+
+  marks[2][0]=90;
+  marks[2][1]=80;
+  marks[2][2]=70;
+
+  for(int row=0;row<3;row++){
+      for(int col=0;col<3;col++){
+            System.out.println(marks[row][col]);
+      }
+      System.out.print(" ");
+  }
+      
+      }
+
+}
